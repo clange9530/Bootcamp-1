916b274728cd4154c7ab78132e1d536eb072bf7f
clange9530 <clange9530@gmail.com>
2018-09-03 16:41:28 -0400

    Updated listingsController.

--- listingController.js
+++ listingController.js
@@ -1,14 +1,25 @@
-angular.module('listings').controller('ListingsController', ['$scope', 'Listings', 
+angular.module('listings').controller('ListingsController', ['$scope', 'Listings',
   function($scope, Listings) {
     $scope.listings = Listings;
     $scope.detailedInfo = undefined;
 
-    /* 
-      Implement these functions in the controller to make your application function 
-      as described in the assignment spec. 
+    /*
+      Implement these functions in the controller to make your application function
+      as described in the assignment spec.
      */
-    $scope.addListing = function() {};
-    $scope.deleteListing = function(index) {};
-    $scope.showDetails = function(index) {};
+    $scope.addListing = function() {
+
+    };
+
+    $scope.deleteListing = function(index) {
+      var deletedListing = $scope.listings.indexOf(index);
+      $scope.listings.splice(deletedListing, 1);
+    };
+
+    $scope.showDetails = function(index) {
+      $scope.detailedInfo = index;
+
+
+    };
   }
 ]);
