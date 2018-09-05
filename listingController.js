a0b1ff9e3316fa321f4d6789a611a29b4aecd209
clange9530 <clange9530@gmail.com>
2018-09-05 14:31:53 -0400

    Updated description in controller.js

--- listingController.js
+++ listingController.js
@@ -2,9 +2,10 @@ angular.module('listings').controller('ListingsController', ['$scope', 'Listings
   function($scope, Listings) {
     $scope.listings = Listings;
     $scope.detailedInfo = undefined;
-    $scope.newlisting;
+    
     /*
-      
+      addListing creates a new object and then populates the fields with the input from the form.
+      After that it pushes it onto the existing array in listings.
       
       deleteListing takes the index of the selected listing and "splices" together the array at 
       the next index, skipping the selected object. 
@@ -12,6 +13,10 @@ angular.module('listings').controller('ListingsController', ['$scope', 'Listings
       showDetails simply puts the selected object from the array into a variable that is easier
       and cleaner to access.
      */
+
+    $scope.newlisting;
+
+
     $scope.addListing = function() {
       var newListing = {
         code: '',
